cd07d5e2e0a007df5a44d7dfe50745b71afe1cce
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-05-20 16:48:51 +0200

    search page - WIP, mongoose error

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -3,17 +3,40 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 const Post = require("../models/Post.model");
+const Quiz = require('../models/Quiz.model');
 const User = require('../models/User.model');
-const Comment = require('../models/Comment.model');
+const Comment = require("../models/Comment.model")
 
 
 router.get('/', (req, res, next) => {
-  res.json("Search page loaded");
+  
+  const { keyword } = req.query;
+
+  Post.find({ $or: [
+    {'title': { $regex: keyword }}, 
+    {'gameName': { $regex: keyword }},
+    {'genre': { $regex: keyword }},
+    {'review': { $regex: keyword }}
+  ]})
+  /* .then(results => {
+    Quiz.find({ $or: [
+      {'question': { $regex: keyword }}, 
+      {'genre': { $regex: keyword }}
+    ]})
+    .then(results => {
+      User.find({ $or: [
+        {'name': { $regex: keyword }}, 
+        {'description': { $regex: keyword }}
+      ]})
+      .then(results => {
+        Comment.find({ $or: [
+          {'message': { $regex: keyword }}
+        ]}) */
+        .then(results => res.status(200).json(results))
+      
+    
+  .catch((err) => res.json(err))
 })
-
-
-
-
 
 
 module.exports = router;
